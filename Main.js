22360018994e8709ba6b3b1b1df8e0b3b5e65071
hosny-ishtaya <hosnyish812@gmail.com>
2023-06-04 22:24:15 +0300

    complete project

--- Main.js
+++ Main.js
@@ -1,49 +1,65 @@
-import {updateMovie,addMovie,deleteMovie,filterMovie} from './Request.js'
-import {readMovie} from './Request.js'
+import { updateMovie, addMovie, deleteMovie, filterMovie, fetchData } from './Request.js'
+import { readMovie } from './Request.js'
 import prompt from 'prompt-sync';
-const input=prompt();
+const input = prompt();
 
-const printmenu=()=>{
+const printmenu = () => {
   console.log(`-----Cenema-----
 1)Read Movies.
 2)Add Movie.
 3)Update Movie.
 4)Delete Movie.
-5)Search Movies
+5)Search Movies.
+6)Fetch data API.
 9)Exit.
 ----------------`)
 
 }
 
- const Main=()=>{
-  let exit=0;
- while(true){
-  printmenu();
-  const choice =input("What your Choice?:")
-  switch(choice){
-  case "1":
-    readMovie()
-     break;
-  case "2":
-    addMovie()
-    break;
-  case "3":
-    updateMovie();
-    break;
-  case "4":
-    deleteMovie();
-    break;
-  case "5":
-    filterMovie();
-    break;     
-   case "9":
-    exit=1;
-    break;  
- }
+async function executeAsyncFunctions() {
+  let userInput = '';
 
- if(exit==1) break
+  while (userInput !== 'exit') {
+    printmenu();
+    userInput = input('Enter an option or "exit" to quit:');
+
+    switch (userInput) {
+      case "1":
+        readMovie()
+        break;
+      case "2":
+        addMovie()
+        break;
+      case "3":
+        updateMovie();
+        break;
+      case "4":
+        deleteMovie();
+        break;
+      case "5":
+        filterMovie();
+        break;
+      case "6":
+        await fetchData();
+        break;
+      case '9':
+      case 'exit':
+        console.log('Exiting...');
+        break;
+      default:
+        console.log('Invalid option');
+    }
+  }
+
+  console.log('Loop exited');
 }
 
-}
+executeAsyncFunctions()
+  .then(() => {
+    console.log('All async functions completed');
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
+  });
 
-Main();
+
--- Request.js
+++ Request.js
@@ -1,8 +1,5 @@
 import fs from 'fs'
-import { filterr, printMoveis } from './Operations.js';
-import {inputMoveis} from './Operations.js';
-import {update} from './Operations.js';
-import { deletee } from './Operations.js';
+import { filterr, printMoveis,inputMoveis,update,deletee} from './Operations.js';
 
 
 const readMovie=()=>{
@@ -42,10 +39,41 @@ const filterMovie=()=>{
 
 }
 
+const fetchData=async ()=>{
+
+   await fetch("https://www.omdbapi.com/?apikey=a61cc832&t=spider+man",{method:"Get"})
+    .then(async(res)=>{
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        const data= await res.json()
+        console.log(res.status);
+        console.log(data.Title)
+
+        let mov = {
+            "name": data.Title,
+            "director": data.Director,
+            "relase_year": data.Year,
+            "genre": data.Genre
+        }
+
+        let dataa=fs.readFileSync("Movies.json","utf-8");
+        let objData=JSON.parse(dataa)
+        objData.movies.push(mov)
+   
+        fs.writeFileSync("Movies.json",JSON.stringify(objData),"utf-8")
+
+    }).catch((err)=>{
+        console.log(err.data);
+    })
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+   
+}
+
 export{
     readMovie,
     addMovie,
     updateMovie,
     deleteMovie,
-    filterMovie
+    filterMovie,
+    fetchData,
 }
